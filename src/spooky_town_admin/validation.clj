f98064e2598ea28b95b37a4f66184704447d9bf2
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-24 13:54:38 +0900

    feat: 선택적 필드 (출판사, 출판일, 가격, 쪽수, 설명) 검증 추가
    - 선택적 필드에 대한 검증 함수 추가
    - validate-comic 함수에서 선택적 필드 처리
    - 선택적 필드에 대한 단위 테스트 추가 및 수정

--- src/spooky_town_admin/validation.clj
+++ src/spooky_town_admin/validation.clj
@@ -19,6 +19,7 @@
 (def validate-title (validate-length "제목" 1 100))
 (def validate-artist (validate-length "그림 작가" 1 20))
 (def validate-author (validate-length "글 작가" 1 20))
+(def validate-publisher (validate-length "출판사" 1 50))
 
 (defn validate-isbn-13 [isbn]
   (if (and (string? isbn)
@@ -32,21 +33,59 @@
     (success isbn)
     (failure {"ISBN-10" "유효하지 않은 ISBN-10 형식입니다."})))
 
+(defn validate-publication-date [date]
+  (if (and (string? date)
+           (re-matches #"^\d{4}-\d{2}-\d{2}$" date))
+    (success date)
+    (failure {"출판일" "유효하지 않은 출판일 형식입니다. YYYY-MM-DD 형식이어야 합니다."})))
+
+(defn validate-price [price]
+  (if (and (number? price) (>= price 0))
+    (success price)
+    (failure {"가격" "가격은 0 이상의 숫자여야 합니다."})))
+
+(defn validate-pages [pages]
+  (if (and (integer? pages) (> pages 0))
+    (success pages)
+    (failure {"쪽수" "쪽수는 1 이상의 정수여야 합니다."})))
+
+(def validate-description (validate-length "설명" 0 1000))
+
+(defn validate-optional-field [field-name validator]
+  (fn [value]
+    (if (nil? value)
+      (success nil)
+      (validator value))))
+
 (defn validate-comic [comic]
   (let [title-result (validate-title (:title comic))
         artist-result (validate-artist (:artist comic))
         author-result (validate-author (:author comic))
         isbn-13-result (validate-isbn-13 (:isbn-13 comic))
-        isbn-10-result (validate-isbn-10 (:isbn-10 comic))]
-    (cond
-      (not (success? title-result)) (failure (:error title-result))
-      (not (success? artist-result)) (failure (:error artist-result))
-      (not (success? author-result)) (failure (:error author-result))
-      (not (success? isbn-13-result)) (failure (:error isbn-13-result))
-      (not (success? isbn-10-result)) (failure (:error isbn-10-result))
-      :else (success (assoc comic 
-                            :title (:value title-result)
-                            :artist (:value artist-result)
-                            :author (:value author-result)
-                            :isbn-13 (:value isbn-13-result)
-                            :isbn-10 (:value isbn-10-result))))))
+        isbn-10-result (validate-isbn-10 (:isbn-10 comic))
+        publisher-result ((validate-optional-field "출판사" validate-publisher) (:publisher comic))
+        publication-date-result ((validate-optional-field "출판일" validate-publication-date) (:publication-date comic))
+        price-result ((validate-optional-field "가격" validate-price) (:price comic))
+        pages-result ((validate-optional-field "쪽수" validate-pages) (:pages comic))
+        description-result ((validate-optional-field "설명" validate-description) (:description comic))
+        all-results [title-result artist-result author-result isbn-13-result isbn-10-result
+                     publisher-result publication-date-result price-result pages-result description-result]
+        errors (reduce (fn [acc result]
+                         (if (success? result)
+                           acc
+                           (merge acc (:error result))))
+                       {}
+                       all-results)]
+    (if (empty? errors)
+      (success (-> comic
+                   (assoc :title (:value title-result))
+                   (assoc :artist (:value artist-result))
+                   (assoc :author (:value author-result))
+                   (assoc :isbn-13 (:value isbn-13-result))
+                   (assoc :isbn-10 (:value isbn-10-result))
+                   (assoc :publisher (:value publisher-result))
+                   (assoc :publication-date (:value publication-date-result))
+                   (assoc :price (:value price-result))
+                   (assoc :pages (:value pages-result))
+                   (assoc :description (:value description-result))))
+      (failure errors))))
--- test/spooky_town_admin/validation_test.clj
+++ test/spooky_town_admin/validation_test.clj
@@ -1,104 +1,87 @@
-(ns spooky-town-admin.validation-test
+ (ns spooky-town-admin.validation-test
   (:require [clojure.test :refer [deftest is testing]]
-            [spooky-town-admin.validation :refer [success? validate-title validate-artist validate-author validate-isbn-13 validate-isbn-10 validate-comic]]))
-
-(deftest validate-title-test
-  (testing "유효한 제목"
-    (let [result (validate-title "유효한 제목")]
-      (is (success? result))
-      (is (= "유효한 제목" (:value result)))))
-  
-  (testing "빈 제목"
-    (let [result (validate-title "")]
-      (is (not (success? result)))
-      (is (= {"제목" "제목의 길이는 1에서 100 사이여야 합니다."} (:error result)))))
-  
-  (testing "너무 긴 제목"
-    (let [result (validate-title (apply str (repeat 101 "a")))]
-      (is (not (success? result)))
-      (is (= {"제목" "제목의 길이는 1에서 100 사이여야 합니다."} (:error result))))))
-
-(deftest validate-artist-test
-  (testing "유효한 그림 작가"
-    (let [result (validate-artist "홍길동")]
-      (is (success? result))
-      (is (= "홍길동" (:value result)))))
-  
-  (testing "빈 그림 작가"
-    (let [result (validate-artist "")]
-      (is (not (success? result)))
-      (is (= {"그림 작가" "그림 작가의 길이는 1에서 20 사이여야 합니다."} (:error result)))))
-  
-  (testing "너무 긴 그림 작가"
-    (let [result (validate-artist (apply str (repeat 21 "a")))]
-      (is (not (success? result)))
-      (is (= {"그림 작가" "그림 작가의 길이는 1에서 20 사이여야 합니다."} (:error result))))))
-
-(deftest validate-author-test
-  (testing "유효한 글 작가"
-    (let [result (validate-author "김철수")]
-      (is (success? result))
-      (is (= "김철수" (:value result)))))
-  
-  (testing "빈 글 작가"
-    (let [result (validate-author "")]
-      (is (not (success? result)))
-      (is (= {"글 작가" "글 작가의 길이는 1에서 20 사이여야 합니다."} (:error result)))))
-  
-  (testing "너무 긴 글 작가"
-    (let [result (validate-author (apply str (repeat 21 "a")))]
-      (is (not (success? result)))
-      (is (= {"글 작가" "글 작가의 길이는 1에서 20 사이여야 합니다."} (:error result))))))
-
-(deftest validate-isbn-13-test
-  (testing "유효한 ISBN-13"
-    (let [result (validate-isbn-13 "978-3-16-148410-0")]
-      (is (success? result))
-      (is (= "978-3-16-148410-0" (:value result)))))
-  
-  (testing "유효하지 않은 ISBN-13"
-    (let [result (validate-isbn-13 "978-3-16-14841")]
-      (is (not (success? result)))
-      (is (= {"ISBN-13" "유효하지 않은 ISBN-13 형식입니다."} (:error result))))))
-
-(deftest validate-isbn-10-test
-  (testing "유효한 ISBN-10"
-    (let [result (validate-isbn-10 "0-306-40615-2")]
-      (is (success? result))
-      (is (= "0-306-40615-2" (:value result)))))
-  
-  (testing "유효하지 않은 ISBN-10"
-    (let [result (validate-isbn-10 "0-306-4061")]
-      (is (not (success? result)))
-      (is (= {"ISBN-10" "유효하지 않은 ISBN-10 형식입니다."} (:error result))))))
+            [spooky-town-admin.validation :refer [success? validate-comic]]))
 
 (deftest validate-comic-test
-  (testing "유효한 만화"
-    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author "김철수" :isbn-13 "978-1-23456-789-0" :isbn-10 "12345-67890-12345-X"})]
+  (testing "유효한 만화 (필수 필드만)"
+    (let [result (validate-comic {:title "유효한 제목" 
+                                  :artist "홍길동" 
+                                  :author "김철수" 
+                                  :isbn-13 "978-1-23456-789-0" 
+                                  :isbn-10 "1-23456-789-0"})]
       (is (success? result))
-      (is (= {:title "유효한 제목" :artist "홍길동" :author "김철수" :isbn-13 "978-1-23456-789-0" :isbn-10 "12345-67890-12345-X"} (:value result)))))
-  
+      (is (= {:title "유효한 제목" 
+              :artist "홍길동" 
+              :author "김철수" 
+              :isbn-13 "978-1-23456-789-0" 
+              :isbn-10 "1-23456-789-0"
+              :publisher nil
+              :publication-date nil
+              :price nil
+              :pages nil
+              :description nil} (:value result)))))
+
+  (testing "유효한 만화 (모든 필드 포함)"
+    (let [result (validate-comic {:title "유효한 제목" 
+                                  :artist "홍길동" 
+                                  :author "김철수" 
+                                  :isbn-13 "978-1-23456-789-0" 
+                                  :isbn-10 "1-23456-789-0"
+                                  :publisher "좋은출판사"
+                                  :publication-date "2023-05-15"
+                                  :price 15000
+                                  :pages 200
+                                  :description "재미있는 만화입니다."})]
+      (is (success? result))
+      (is (= {:title "유효한 제목" 
+              :artist "홍길동" 
+              :author "김철수" 
+              :isbn-13 "978-1-23456-789-0" 
+              :isbn-10 "1-23456-789-0"
+              :publisher "좋은출판사"
+              :publication-date "2023-05-15"
+              :price 15000
+              :pages 200
+              :description "재미있는 만화입니다."} (:value result)))))
+
   (testing "유효하지 않은 만화 - 제목 오류"
-    (let [result (validate-comic {:title "" :artist "홍길동" :author "김철수"})]
+    (let [result (validate-comic {:title "" :artist "홍길동" :author "김철수" :isbn-13 "978-1-23456-789-0" :isbn-10 "1-23456-789-0"})]
       (is (not (success? result)))
       (is (= {"제목" "제목의 길이는 1에서 100 사이여야 합니다."} (:error result)))))
 
   (testing "유효하지 않은 만화 - 그림 작가 오류"
-    (let [result (validate-comic {:title "유효한 제목" :artist "" :author "김철수"})]
+    (let [result (validate-comic {:title "유효한 제목" :artist "" :author "김철수" :isbn-13 "978-1-23456-789-0" :isbn-10 "1-23456-789-0"})]
       (is (not (success? result)))
       (is (= {"그림 작가" "그림 작가의 길이는 1에서 20 사이여야 합니다."} (:error result)))))
 
   (testing "유효하지 않은 만화 - 글 작가 오류"
-    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author ""})]
+    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author "" :isbn-13 "978-1-23456-789-0" :isbn-10 "1-23456-789-0"})]
       (is (not (success? result)))
       (is (= {"글 작가" "글 작가의 길이는 1에서 20 사이여야 합니다."} (:error result)))))
 
   (testing "유효하지 않은 만화 - ISBN-13 오류"
-    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author "김철수" :isbn-13 "invalid-isbn", :isbn-10 "12345-67890-12345-X"})]
+    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author "김철수" :isbn-13 "invalid-isbn" :isbn-10 "1-23456-789-0"})]
       (is (not (success? result)))
       (is (= {"ISBN-13" "유효하지 않은 ISBN-13 형식입니다."} (:error result)))))
 
   (testing "유효하지 않은 만화 - ISBN-10 오류"
-    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author "김철수" :isbn-13 "978-3-16-148410-0" :isbn-10 "invalid-isbn"})]
+    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author "김철수" :isbn-13 "978-1-23456-789-0" :isbn-10 "invalid-isbn"})]
       (is (not (success? result)))
-      (is (= {"ISBN-10" "유효하지 않은 ISBN-10 형식입니다."} (:error result))))))
+      (is (= {"ISBN-10" "유효하지 않은 ISBN-10 형식입니다."} (:error result)))))
+
+  (testing "유효하지 않은 만화 - 선택적 필드 오류"
+    (let [result (validate-comic {:title "유효한 제목" 
+                                  :artist "홍길동" 
+                                  :author "김철수" 
+                                  :isbn-13 "978-1-23456-789-0" 
+                                  :isbn-10 "1-23456-789-0"
+                                  :publisher "좋은출판사"
+                                  :publication-date "2023/05/15"
+                                  :price -1000
+                                  :pages 0
+                                  :description (apply str (repeat 1001 "a"))})]
+      (is (not (success? result)))
+      (is (contains? (:error result) "출판일"))
+      (is (contains? (:error result) "가격"))
+      (is (contains? (:error result) "쪽수"))
+      (is (contains? (:error result) "설명")))))
