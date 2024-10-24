937457b4db24d71772da8b457ee11daf067f7956
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-24 18:46:44 +0900

    test: 인프라스트럭처 계층 테스트 구현
    - 저장소(persistence) 테스트
      - 만화 저장/조회/삭제/목록 기능 검증
      - 테스트 격리를 위한 fixture 구현
      - 각 기능별 독립적인 테스트 케이스 작성
    
    - 이미지 저장소(image-storage) 테스트
      - Mock CDN 저장소 기능 검증
      - 이미지 메타데이터 추출 테스트
      - 환경별 저장소 팩토리 테스트
      - 테스트용 이미지 생성 헬퍼 구현

---
+++ test/spooky_town_admin/infrastructure/persistence_test.clj
@@ -0,0 +1,61 @@
+(ns spooky-town-admin.infrastructure.persistence-test
+  (:require [clojure.test :refer [deftest testing is use-fixtures]]
+            [spooky-town-admin.infrastructure.persistence :as persistence]))
+
+;; 테스트 픽스처 - 각 테스트 전에 DB 상태 초기화
+(defn reset-db-fixture [f]
+  (reset! persistence/db-state {:comics {} :next-id 1})
+  (f))
+
+(use-fixtures :each reset-db-fixture)
+
+(def test-comic
+  {:title "테스트 만화"
+   :artist "테스트 작가"
+   :author "테스트 글작가"
+   :isbn13 "978-1-23456-789-0"
+   :isbn10 "1-23456-789-0"
+   :publisher "테스트 출판사"
+   :price 15000})
+
+(deftest save-comic-test
+  (testing "만화 저장"
+    (let [repo (persistence/create-comic-repository)
+          result (persistence/save-comic repo test-comic)]
+      (is (:success result))
+      (is (= 1 (:id result))))))
+
+(deftest find-comic-by-id-test
+  (testing "ID로 만화 조회"
+    (let [repo (persistence/create-comic-repository)]
+      (persistence/save-comic repo test-comic)
+      (let [comic (persistence/find-comic-by-id repo 1)]
+        (is (some? comic))
+        (is (= "테스트 만화" (:title comic)))
+        (is (= 1 (:id comic)))))))
+
+(deftest find-comic-by-isbn-test
+  (testing "ISBN으로 만화 조회"
+    (let [repo (persistence/create-comic-repository)]
+      (persistence/save-comic repo test-comic)
+      (let [comic (persistence/find-comic-by-isbn repo "978-1-23456-789-0")]
+        (is (some? comic))
+        (is (= "테스트 만화" (:title comic)))))))
+
+(deftest delete-comic-test
+  (testing "만화 삭제"
+    (let [repo (persistence/create-comic-repository)]
+      (persistence/save-comic repo test-comic)
+      (let [delete-result (persistence/delete-comic repo 1)]
+        (is (:success delete-result))
+        (is (nil? (persistence/find-comic-by-id repo 1)))))))
+
+(deftest list-comics-test
+  (testing "만화 목록 조회"
+    (let [repo (persistence/create-comic-repository)]
+      (persistence/save-comic repo test-comic)
+      (persistence/save-comic repo (assoc test-comic :title "테스트 만화 2"))
+      (let [comics (persistence/list-comics repo)]
+        (is (= 2 (count comics)))
+        (is (= #{"테스트 만화" "테스트 만화 2"}
+               (set (map :title comics))))))))
