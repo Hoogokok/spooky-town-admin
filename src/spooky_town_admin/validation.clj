09098fef59f847d8d4a55b1ee867366b65416f27
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-24 13:16:09 +0900

    refactor: 그림 작가와 글 작가 필드 검증 추가
    - validate-artist와 validate-author 함수 추가
    - validate-comic 함수에 그림 작가와 글 작가 검증 로직 추가
    - 새로운 검증 로직에 대한 단위 테스트 추가

--- src/spooky_town_admin/validation.clj
+++ src/spooky_town_admin/validation.clj
@@ -16,9 +16,18 @@
       (failure {field-name (str field-name "의 길이는 " min "에서 " max " 사이여야 합니다.")}))))
 
 (def validate-title (validate-length "제목" 1 100))
+(def validate-artist (validate-length "그림 작가" 1 20))
+(def validate-author (validate-length "글 작가" 1 20))
 
 (defn validate-comic [comic]
-  (let [title-result (validate-title (:title comic))]
-    (if (success? title-result)
-      (success (assoc comic :title (:value title-result)))
-      (failure (:error title-result)))))
+  (let [title-result (validate-title (:title comic))
+        artist-result (validate-artist (:artist comic))
+        author-result (validate-author (:author comic))]
+    (cond
+      (not (success? title-result)) (failure (:error title-result))
+      (not (success? artist-result)) (failure (:error artist-result))
+      (not (success? author-result)) (failure (:error author-result))
+      :else (success (assoc comic 
+                            :title (:value title-result)
+                            :artist (:value artist-result)
+                            :author (:value author-result))))))
--- test/spooky_town_admin/validation_test.clj
+++ test/spooky_town_admin/validation_test.clj
@@ -1,6 +1,6 @@
 (ns spooky-town-admin.validation-test
   (:require [clojure.test :refer [deftest is testing]]
-            [spooky-town-admin.validation :refer [success? validate-title validate-comic]]))
+            [spooky-town-admin.validation :refer [success? validate-title validate-artist validate-author validate-comic]]))
 
 (deftest validate-title-test
   (testing "유효한 제목"
@@ -18,13 +18,55 @@
       (is (not (success? result)))
       (is (= {"제목" "제목의 길이는 1에서 100 사이여야 합니다."} (:error result))))))
 
+(deftest validate-artist-test
+  (testing "유효한 그림 작가"
+    (let [result (validate-artist "홍길동")]
+      (is (success? result))
+      (is (= "홍길동" (:value result)))))
+  
+  (testing "빈 그림 작가"
+    (let [result (validate-artist "")]
+      (is (not (success? result)))
+      (is (= {"그림 작가" "그림 작가의 길이는 1에서 20 사이여야 합니다."} (:error result)))))
+  
+  (testing "너무 긴 그림 작가"
+    (let [result (validate-artist (apply str (repeat 21 "a")))]
+      (is (not (success? result)))
+      (is (= {"그림 작가" "그림 작가의 길이는 1에서 20 사이여야 합니다."} (:error result))))))
+
+(deftest validate-author-test
+  (testing "유효한 글 작가"
+    (let [result (validate-author "김철수")]
+      (is (success? result))
+      (is (= "김철수" (:value result)))))
+  
+  (testing "빈 글 작가"
+    (let [result (validate-author "")]
+      (is (not (success? result)))
+      (is (= {"글 작가" "글 작가의 길이는 1에서 20 사이여야 합니다."} (:error result)))))
+  
+  (testing "너무 긴 글 작가"
+    (let [result (validate-author (apply str (repeat 21 "a")))]
+      (is (not (success? result)))
+      (is (= {"글 작가" "글 작가의 길이는 1에서 20 사이여야 합니다."} (:error result))))))
+
 (deftest validate-comic-test
   (testing "유효한 만화"
-    (let [result (validate-comic {:title "유효한 제목"})]
+    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author "김철수"})]
       (is (success? result))
-      (is (= {:title "유효한 제목"} (:value result)))))
+      (is (= {:title "유효한 제목" :artist "홍길동" :author "김철수"} (:value result)))))
   
-  (testing "유효하지 않은 만화"
-    (let [result (validate-comic {:title ""})]
+  (testing "유효하지 않은 만화 - 제목 오류"
+    (let [result (validate-comic {:title "" :artist "홍길동" :author "김철수"})]
       (is (not (success? result)))
-      (is (= {"제목" "제목의 길이는 1에서 100 사이여야 합니다."} (:error result))))))
+      (is (= {"제목" "제목의 길이는 1에서 100 사이여야 합니다."} (:error result)))))
+
+  (testing "유효하지 않은 만화 - 그림 작가 오류"
+    (let [result (validate-comic {:title "유효한 제목" :artist "" :author "김철수"})]
+      (is (not (success? result)))
+      (is (= {"그림 작가" "그림 작가의 길이는 1에서 20 사이여야 합니다."} (:error result)))))
+
+  (testing "유효하지 않은 만화 - 글 작가 오류"
+    (let [result (validate-comic {:title "유효한 제목" :artist "홍길동" :author ""})]
+      (is (not (success? result)))
+      (is (= {"글 작가" "글 작가의 길이는 1에서 20 사이여야 합니다."} (:error result))))))
