64d8a93ec894f09c750b36529dbbaafbaee7d9fe
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-25 10:53:24 +0900

    feat: CORS 설정 추가

--- src/spooky_town_admin/web/routes.clj
+++ src/spooky_town_admin/web/routes.clj
@@ -8,22 +8,34 @@
 ;; 에러 응답 생성 헬퍼
 (defn- error-response [error]
   (let [status (cond
-                 (instance? errors/ValidationError error) 400
-                 (instance? errors/BusinessError error) 400
-                 (instance? errors/SystemError error) 500
-                 :else 500)]
+                 (instance? spooky_town_admin.domain.comic.errors.ValidationError error) 400
+                 (instance? spooky_town_admin.domain.comic.errors.BusinessError error) 400
+                 (instance? spooky_town_admin.domain.comic.errors.SystemError error) 500
+                 :else 500)
+        error-type (cond
+                    (instance? spooky_town_admin.domain.comic.errors.ValidationError error) "ValidationError"
+                    (instance? spooky_town_admin.domain.comic.errors.BusinessError error) "BusinessError"
+                    (instance? spooky_town_admin.domain.comic.errors.SystemError error) "SystemError"
+                    :else "UnknownError")]
     {:status status
-     :body {:error (type error)
+     :body {:error error-type
             :message (:message error)
-            :details (when (instance? errors/SystemError error)
+            :field (when (instance? spooky_town_admin.domain.comic.errors.ValidationError error)
+                    (:field error))
+            :details (when (instance? spooky_town_admin.domain.comic.errors.SystemError error)
                       (:details error))}}))
 
 ;; 요청 처리 핸들러
-(defn handle-create-comic [service body]
-  (let [result (comic-service/create-comic service body)]
-    (if (:success result)
-      (response {:id (:id result)})
-      (bad-request (error-response (:error result))))))
+(defn handle-create-comic [service request]
+  (let [params (if-let [multipart (:multipart-params request)]
+                 (merge (:params request) multipart)
+                 (:params request))]
+    (println "Received params:" params)  ;; 로깅 추가
+    (let [result (comic-service/create-comic service params)]
+      (println "Service result:" result)  ;; 로깅 추가
+      (if (:success result)
+        (response {:id (:id result)})
+        (bad-request (error-response (:error result)))))))
 
 (defn handle-get-comic [service id]
   (let [result (comic-service/get-comic service id)]
@@ -38,9 +50,9 @@
 ;; 라우트 정의
 (defn create-routes [service]
   (defroutes app-routes
-    ;; 만화 생성
-    (POST "/api/comics" {body :body}
-      (handle-create-comic service body))
+    ;; 만화 생성 - 전체 request를 전달
+    (POST "/api/comics" request
+      (handle-create-comic service request))
     
     ;; 만화 조회
     (GET "/api/comics/:id" [id]
@@ -50,6 +62,8 @@
     (GET "/api/comics" []
       (handle-list-comics service))
     
+    (route/resources "/")
+
     ;; 404 처리
     (route/not-found 
      {:error "Not Found"
