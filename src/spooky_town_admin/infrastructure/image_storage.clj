d44d4891d1d82c849351edca85bcab272e2ddb50
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-27 18:42:44 +0900

    refactor: 이미지 저장소 구현을 Result 타입 기반으로 변경
    - ImageStorage 프로토콜의 모든 메서드가 Result 타입을 반환하도록 수정
    - extract-image-metadata가 Result를 반환하도록 변경
    - MockCDNImageStorage와 CloudinaryImageStorage의 모든 메서드가 Result를 반환하도록 수정
    - try-catch 블록에서 Result 타입으로 에러 처리 통일
    
    이 변경으로 이미지 저장소 구현이 애플리케이션의 함수형 에러 처리 패턴과
    일관성을 가지게 되었으며, 상위 레이어에서 더 예측 가능한 방식으로
    에러를 처리할 수 있게 되었습니다.

--- src/spooky_town_admin/infrastructure/image_storage.clj
+++ src/spooky_town_admin/infrastructure/image_storage.clj
@@ -1,11 +1,15 @@
 (ns spooky-town-admin.infrastructure.image-storage
   (:require [spooky-town-admin.domain.comic.errors :as errors]
+            [spooky-town-admin.domain.common.result :as r]
+            [spooky-town-admin.infrastructure.config.cloudinary :as cloud-config]
             [clojure.java.io :as io])
   (:import [java.io File]
            [java.nio.file Files Path]
            [javax.imageio ImageIO]
            [java.awt.image BufferedImage]
-           [java.util UUID]))
+           [java.util UUID]
+           [com.cloudinary.utils ObjectUtils]
+           [com.cloudinary Cloudinary]))
 
 ;; 이미지 저장소 프로토콜
 (defprotocol ImageStorage
@@ -20,69 +24,91 @@
           ^BufferedImage image (ImageIO/read temp-file)
           ^Path path (.toPath temp-file)
           content-type (Files/probeContentType path)]
-      {:success true
-       :metadata {:width (.getWidth image)
+      (r/success {:width (.getWidth image)
                  :height (.getHeight image)
                  :content-type content-type
-                 :size (.length temp-file)}})
+                 :size (.length temp-file)}))
     (catch Exception e
-      {:success false
-       :error (errors/system-error 
-               :image-processing-error
-               (errors/get-system-message :image-processing-error)
-               (.getMessage e))})))
+      (r/failure (errors/system-error
+                  :image-processing-error
+                  (errors/get-system-message :image-processing-error)
+                  (.getMessage e))))))
 
 
 ;; 테스트용 Mock CDN 저장소 구현
 (defrecord MockCDNImageStorage []
   ImageStorage
   (store-image [_ image-data]
-    (try
-      (let [metadata-result (extract-image-metadata image-data)]
-        (if (:success metadata-result)
-          {:success true
-           :image-id (str (UUID/randomUUID))
-           :metadata (:metadata metadata-result)}
-          metadata-result))
-      (catch Exception e
-        {:success false
-         :error (errors/system-error 
-                :image-upload-error
-                (errors/get-system-message :image-upload-error)
-                (.getMessage e))})))
-  
-  (delete-image [_ image-id]
-    {:success true})
-  
-  (get-image-url [_ image-id]
-    (str "https://mock-cdn.example.com/images/" image-id)))
+    (-> (extract-image-metadata image-data)
+        (r/map (fn [metadata]
+                 {:image-id (str (UUID/randomUUID))
+                  :metadata metadata}))))
 
-;; 실제 Cloudflare R2 저장소 구현 (향후 구현)
-(defrecord CloudflareR2ImageStorage [config]
+  (delete-image [_ image-id]
+    (r/success true))
+
+  (get-image-url [_ image-id]
+    (r/success (str "https://mock-cdn.example.com/images/" image-id))))
+
+;; Cloudinary 저장소 구현소 
+(defrecord CloudinaryImageStorage [cloudinary]
   ImageStorage
   (store-image [_ image-data]
-    ;; TODO: Cloudflare R2 구현
-    )
-  
+    (try
+      (let [temp-file (:tempfile image-data)
+            options (ObjectUtils/asMap (to-array
+                     ["resource_type" "auto"
+                      "unique_filename" true
+                      "overwrite" false]))
+            uploader (.uploader cloudinary)
+            upload-result (.upload uploader temp-file options)
+            public-id (.get upload-result "public_id")]
+        (r/success
+          {:image-id public-id
+           :metadata {:width (.get upload-result "width")
+                     :height (.get upload-result "height")
+                     :content-type (str "image/" (.get upload-result "format"))
+                     :size (.get upload-result "bytes")
+                     :url (.get upload-result "secure_url")}}))
+      (catch Exception e
+        (r/failure (errors/system-error
+                    :image-upload-error
+                    (errors/get-system-message :image-upload-error)
+                    (.getMessage e))))))
+
   (delete-image [_ image-id]
-    ;; TODO: Cloudflare R2 구현
-    )
-  
+    (try
+      (let [options (ObjectUtils/asMap (to-array []))
+            api (.api cloudinary)]
+        (.deleteResources api (into-array String [image-id]) options)
+        (r/success true))
+      (catch Exception e
+        (r/failure (errors/system-error
+                    :image-delete-error
+                    (errors/get-system-message :image-delete-error)
+                    (.getMessage e))))))
+
   (get-image-url [_ image-id]
-    ;; TODO: Cloudflare R2 구현
-    ))
+    (try
+      (let [url (.url cloudinary)]
+        (r/success (.generate url image-id)))
+      (catch Exception e
+        (r/failure (errors/system-error
+                    :image-url-error
+                    (errors/get-system-message :image-url-error)
+                    (.getMessage e)))))))
 
 ;; 저장소 팩토리 함수들
 (defn create-mock-image-storage []
   (->MockCDNImageStorage))
 
-(defn create-r2-image-storage [config]
-  (->CloudflareR2ImageStorage config))
+(defn create-cloudinary-storage []
+  (->CloudinaryImageStorage (cloud-config/create-cloudinary)))
 
 ;; 환경에 따른 저장소 생성
 (defn create-image-storage [env config]
   (case env
     :test (create-mock-image-storage)
-    :prod (create-r2-image-storage config)
+    :prod (create-cloudinary-storage)
     ;; 기본값은 mock 저장소
     (create-mock-image-storage)))
