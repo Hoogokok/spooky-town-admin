4e288aaaffd45dadb96837d21a6c7cbf8fbe6d26
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-28 14:34:58 +0900

    refactor: 이미지 처리 관련 함수 제거 및 저장소 계층 리팩토링
    - 이미지 처리 관련 함수들을 제거하고 저장소 계층을 리팩토링했습니다.
    - 이 변경은 이미지 처리 워크플로우를 단순화하고 코드를 더 깔끔하게 만들었습니다.

--- src/spooky_town_admin/application/comic_service.clj
+++ src/spooky_town_admin/application/comic_service.clj
@@ -1,31 +1,13 @@
 (ns spooky-town-admin.application.comic-service
   (:require [spooky-town-admin.domain.comic.workflow :as workflow]
             [spooky-town-admin.domain.comic.errors :as errors]
-            [spooky-town-admin.infrastructure.persistence :as persistence]
+            [spooky-town-admin.domain.comic.types :as types]
             [spooky-town-admin.infrastructure.image-storage :as image-storage]
+            [spooky-town-admin.infrastructure.persistence :as persistence]
             [spooky-town-admin.domain.common.result :as r]))
 
+
 (defrecord ComicService [comic-repository image-storage])
-
-;; 이미지 처리 관련 함수들
-(defn- extract-and-validate-metadata [image-data]
-  (if-let [metadata (workflow/extract-image-metadata image-data)]
-    (workflow/validate-image-constraints metadata)
-    (r/failure (errors/validation-error :cover-image 
-                                      (errors/get-image-error-message :invalid)))))
-
-(defn- store-image [image-storage image-data metadata]
-  (-> (image-storage/store-image image-storage image-data)
-      (r/map-error #(errors/validation-error :cover-image 
-                                           (errors/get-image-error-message :invalid)))
-      (r/bind #(r/success {:image-id (:image-id %) :metadata metadata}))))
-
-(defn- process-cover-image [image-storage image-data]
-  (if image-data
-    (-> (extract-and-validate-metadata image-data)
-        (r/bind #(store-image image-storage image-data %)))
-    (r/success nil)))
-
 ;; 만화 생성 관련 함수들
 (defn- check-duplicate-isbn [comic-repository comic-data]
   (if-let [existing-comic (persistence/find-comic-by-isbn 
@@ -35,37 +17,34 @@
                                     (errors/get-business-message :duplicate-isbn)))
     (r/success comic-data)))
 
-(defn- save-comic [comic-repository comic]
+(defn- save-comic [comic-repository comic image-url]
   (try
-    (r/success (persistence/save-comic comic-repository comic))
+    (let [persisted (persistence/save-comic comic-repository 
+                     (-> comic
+                         (update :title str)  ;; Title 레코드를 문자열로 변환
+                         (update :artist str)  ;; Artist 레코드를 문자열로 변환
+                         (update :author str)  ;; Author 레코드를 문자열로 변환
+                         (update :isbn13 str)  ;; ISBN13 레코드를 문자열로 변환
+                         (update :isbn10 str)  ;; ISBN10 레코드를 문자열로 변환
+                         (assoc :image-url image-url)))]
+      (r/success persisted))
     (catch Exception e
       (r/failure (errors/system-error :db-error 
                                     (errors/get-system-message :db-error)
                                     (.getMessage e))))))
 
-(defn- create-comic-with-image [comic-repository image-storage comic-data image]
-  (-> (process-cover-image image-storage image)
-      (r/bind #(r/success (assoc comic-data 
-                                :cover-image image
-                                :cover-image-metadata (:metadata %))))
-      (r/bind workflow/create-comic-workflow)
-      (r/bind #(save-comic comic-repository %))))
-
-(defn- create-comic-without-image [comic-repository comic-data]
-  (-> (r/success comic-data)
-      (r/bind workflow/create-comic-workflow)
-      (r/bind #(save-comic comic-repository %))))
-
-;; 만화 생성 서비스
 (defn create-comic [{:keys [comic-repository image-storage]} comic-data]
   (-> (persistence/with-transaction
         (-> (r/success comic-data)
             (r/bind #(check-duplicate-isbn comic-repository %))
-            (r/bind #(if-let [image (:cover-image %)]
-                      (create-comic-with-image comic-repository image-storage % image)
-                      (create-comic-without-image comic-repository %)))
-            ;; 성공 시 id만 추출
-            (r/map #(select-keys % [:id]))))
+            (r/bind #(workflow/create-comic-workflow image-storage %))
+            (r/bind (fn [{:keys [comic events image-url]}]
+                     (-> (save-comic comic-repository comic image-url)
+                         (r/map (fn [persisted]
+                                 {:id (:id persisted)
+                                  :events (conj events 
+                                              (types/create-comic-persisted persisted))})))))))
+      (r/map #(select-keys % [:id]))
       r/to-map))
 
 ;; 만화 조회 서비스
@@ -79,9 +58,10 @@
 
 ;; 만화 목록 조회 서비스
 (defn list-comics [{:keys [comic-repository]}]
-  {:success true
-   :comics (map #(select-keys % [:id :title :artist :author :isbn13 :isbn10])
-                (persistence/list-comics comic-repository))})
+  (let [comics (persistence/list-comics comic-repository)]
+    {:success true
+     :comics (map #(select-keys % [:id :title :artist :author :isbn13 :isbn10])
+                  comics)}))
 
 ;; 서비스 인스턴스 생성
 (defn create-comic-service [env config]
