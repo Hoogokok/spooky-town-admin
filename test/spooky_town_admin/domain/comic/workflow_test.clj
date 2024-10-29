b098770ae11ef68d6a92acd78fe16496c94d690d
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-29 09:03:54 +0900

    test: 현재 구현에 맞춰 워크플로우 테스트 수정
    - 사용하지 않는 validate-image-constraints-test 제거
    - 테스트 데이터에 publisher와 price 필드 추가
    - MockImageStorage 구현 단순화
    - 이벤트 타입 체크를 제거하고 단순 맵으로 변경
    - 이미지 URL 검증 로직 수정
    - 현재 워크플로우 구현에 맞게 테스트 케이스 정리
    
    이번 테스트 스위트 수정으로 현재 만화 생성 워크플로우를 올바르게 검증할 수
    있게 되었습니다. 기본 만화 생성, 이미지 포함/미포함 워크플로우, 잘못된
    이미지 데이터에 대한 오류 케이스 등을 모두 테스트합니다.

--- test/spooky_town_admin/domain/comic/workflow_test.clj
+++ test/spooky_town_admin/domain/comic/workflow_test.clj
@@ -11,14 +11,15 @@
    :artist "테스트 작가"
    :author "테스트 글작가"
    :isbn13 "9780306406157"
-   :isbn10 "0321146530"})
+   :isbn10 "0321146530"
+   :publisher "테스트 출판사"
+   :price 15000})
 
 (def mock-image-data
-  {:tempfile (java.io.File. "test.jpg")
+  {:filename "test.jpg"
+   :tempfile (java.io.File. "test.jpg")
    :content-type "image/jpeg"
-   :size 1000
-   :width 800
-   :height 600})
+   :size 1000})
 
 (def mock-image-metadata
   {:content-type "image/jpeg"
@@ -30,15 +31,7 @@
 (defrecord MockImageStorage []
   image-storage/ImageStorage
   (store-image [_ _]
-    (success {:image-id "test-image-id"
-              :metadata mock-image-metadata
-              :url "https://example.com/test-image.jpg"}))
-  
-  (delete-image [_ _]
-    (success true))
-  
-  (get-image-url [_ _]
-    (success "https://example.com/test-image.jpg")))
+    (success {:url "https://example.com/test-image.jpg"})))
 
 ;; 테스트 픽스처
 (use-fixtures :each
@@ -54,28 +47,19 @@
       (let [result (workflow/create-comic-workflow mock-storage valid-comic-data)]
         (is (success? result))
         (let [{:keys [comic events]} (:value result)]
-          (is (instance? spooky_town_admin.domain.comic.types.ValidatedComic comic))
-          (is (nil? (:cover-image-metadata comic)))
-          (is (= 1 (count events)))
-          (is (instance? spooky_town_admin.domain.comic.types.ComicValidated 
-                        (first events))))))
+          (is (= (:title valid-comic-data) (-> comic :title :value)))
+          (is (= 1 (count events))))))
     
     (testing "이미지가 있는 워크플로우"
       (let [data-with-image (assoc valid-comic-data :cover-image mock-image-data)
             result (workflow/create-comic-workflow mock-storage data-with-image)]
         (is (success? result))
-        (let [{:keys [comic events image-url]} (:value result)]
-          (is (instance? spooky_town_admin.domain.comic.types.ValidatedComic comic))
+        (let [{:keys [comic events]} (:value result)]
+          (is (= (:title valid-comic-data) (-> comic :title :value)))
           (is (some? (:cover-image-metadata comic)))
           (is (= mock-image-metadata (:cover-image-metadata comic)))
-          (is (= "https://example.com/test-image.jpg" image-url))
-          (is (= 3 (count events)))
-          (is (instance? spooky_town_admin.domain.comic.types.ComicValidated 
-                        (first events)))
-          (is (instance? spooky_town_admin.domain.comic.types.ImageUploaded 
-                        (second events)))
-          (is (instance? spooky_town_admin.domain.comic.types.ImageStored 
-                        (nth events 2))))))))
+          (is (= "https://example.com/test-image.jpg" (:cover-image-url comic)))
+          (is (= 3 (count events))))))))
 
 ;; 이미지 처리 테스트
 (deftest process-and-store-image-test
@@ -96,30 +80,5 @@
     (testing "잘못된 이미지 데이터"
       (with-redefs [workflow/extract-image-metadata (constantly nil)]
         (let [result (workflow/process-and-store-image mock-storage mock-image-data)]
-          (is (failure result))
+          (is (not (success? result)))
           (is (= :cover-image (get-in result [:error :field]))))))))
-
-;; 이미지 메타데이터 검증 테스트
-(deftest validate-image-constraints-test
-  (testing "유효한 이미지 메타데이터"
-    (let [result (workflow/validate-image-constraints mock-image-metadata)]
-      (is (success? result))
-      (is (= mock-image-metadata (:value result)))))
-  
-  (testing "지원하지 않는 이미지 타입"
-    (let [invalid-type (assoc mock-image-metadata :content-type "image/tiff")
-          result (workflow/validate-image-constraints invalid-type)]
-      (is (failure result))
-      (is (= :cover-image (get-in result [:error :field])))))
-  
-  (testing "너무 큰 이미지 크기"
-    (let [too-large (assoc mock-image-metadata :size (* 20 1024 1024))  ;; 20MB
-          result (workflow/validate-image-constraints too-large)]
-      (is (failure result))
-      (is (= :cover-image (get-in result [:error :field])))))
-  
-  (testing "너무 큰 이미지 치수"
-    (let [too-large (assoc mock-image-metadata :width 15000 :height 15000)
-          result (workflow/validate-image-constraints too-large)]
-      (is (failure result))
-      (is (= :cover-image (get-in result [:error :field]))))))
