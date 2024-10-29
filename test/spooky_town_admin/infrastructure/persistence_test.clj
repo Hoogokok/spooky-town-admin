62e4ec735918cee20935fb839c22cc4b788e235a
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-29 09:00:31 +0900

    refactor: 테스트 환경 설정을 위한 픽스처 추가

--- test/spooky_town_admin/infrastructure/persistence_test.clj
+++ test/spooky_town_admin/infrastructure/persistence_test.clj
@@ -1,13 +1,20 @@
 (ns spooky-town-admin.infrastructure.persistence-test
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
             [spooky-town-admin.infrastructure.persistence :as persistence]
-            [spooky-town-admin.infrastructure.persistence.in-memory :as in-memory]))
+            [spooky-town-admin.infrastructure.persistence.in-memory :as in-memory]
+            [environ.core :refer [env]]))
 
 ;; 테스트 픽스처 - 각 테스트 전에 DB 상태 초기화
 (defn reset-db-fixture [f]
   (reset! in-memory/db-state {:comics {} :next-id 1})
   (f))
 
+;; 테스트 환경 설정을 위한 픽스처
+(defn with-test-env [f]
+  (with-redefs [env (assoc env :environment "test")]
+    (f)))
+
+(use-fixtures :once with-test-env)  ;; 모든 테스트에 환경 설정 적용
 (use-fixtures :each reset-db-fixture)
 
 (def test-comic
@@ -21,14 +28,14 @@
 
 (deftest save-comic-test
   (testing "만화 저장"
-    (let [repo (persistence/create-comic-repository :test)
+    (let [repo (persistence/create-comic-repository)
           result (persistence/save-comic repo test-comic)]
       (is (:success result))
       (is (= 1 (:id result))))))
 
 (deftest find-comic-by-id-test
   (testing "ID로 만화 조회"
-    (let [repo (persistence/create-comic-repository :test)]
+    (let [repo (persistence/create-comic-repository)]
       (persistence/save-comic repo test-comic)
       (let [comic (persistence/find-comic-by-id repo 1)]
         (is (some? comic))
@@ -37,7 +44,7 @@
 
 (deftest find-comic-by-isbn-test
   (testing "ISBN으로 만화 조회"
-    (let [repo (persistence/create-comic-repository :test)]
+    (let [repo (persistence/create-comic-repository)]
       (persistence/save-comic repo test-comic)
       (let [comic (persistence/find-comic-by-isbn repo "9780306406157")]
         (is (some? comic))
@@ -45,7 +52,7 @@
 
 (deftest delete-comic-test
   (testing "만화 삭제"
-    (let [repo (persistence/create-comic-repository :test)]
+    (let [repo (persistence/create-comic-repository)]
       (persistence/save-comic repo test-comic)
       (let [delete-result (persistence/delete-comic repo 1)]
         (is (:success delete-result))
@@ -53,7 +60,7 @@
 
 (deftest list-comics-test
   (testing "만화 목록 조회"
-    (let [repo (persistence/create-comic-repository :test)]
+    (let [repo (persistence/create-comic-repository)]
       (persistence/save-comic repo test-comic)
       (persistence/save-comic repo (assoc test-comic :title "테스트 만화 2"))
       (let [comics (persistence/list-comics repo)]
