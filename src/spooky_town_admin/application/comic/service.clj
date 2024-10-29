37534b5ceb09a718a0eabedebccfae001ca8b71a
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-29 19:14:32 +0900

    feat: 출판사 저장소 추가

--- src/spooky_town_admin/application/comic/service.clj
+++ src/spooky_town_admin/application/comic/service.clj
@@ -3,9 +3,10 @@
    [spooky-town-admin.infrastructure.image-storage :as image-storage]
    [spooky-town-admin.infrastructure.persistence :as persistence]))
 
-(defrecord ComicService [comic-repository image-storage])
+(defrecord ComicService [comic-repository publisher-repository image-storage])
 
 (defn create-comic-service [env]
   (->ComicService 
    (persistence/create-comic-repository)
+   (persistence/create-publisher-repository)
    (image-storage/create-image-storage env))) 
